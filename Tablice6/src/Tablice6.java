b9e39238f0a1245cd4b35ee8dcf8f606a29bf3cf
Dziben <56271191+Dziben@users.noreply.github.com>
2021-03-01 04:11:30 +0100

    Create Tablice6.java

---
+++ Tablice6/src/Tablice6.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Tablice1 {
+    public static void main(String[] args) {
+        double[] power = {23.45, -2.22, 45.90, 67.45, -56, 234.11, -34.8, 90.4, -77};
+        System.out.println();
+        System.out.println("Element [0] = " + power[0]);
+        System.out.println("Element [1] = " + power[1]);
+        System.out.println("Element [2] = " + power[2]);
+        System.out.println("Element [3] = " + power[3]);
+        System.out.println("Element [4] = " + power[4]);
+        System.out.println("Element [5] = " + power[8]);
+        System.out.println("Element [6] = " + power[6]);
+        System.out.println("Element [7] = " + power[7]);
+        System.out.println("Element [8] = " + power[5]);
+        System.out.println();
+        System.out.println("PO ODWROCENIU: ");
+        System.out.println();
+        System.out.println("Element [0] = " + power[5]);
+        System.out.println("Element [1] = " + power[7]);
+        System.out.println("Element [2] = " + power[6]);
+        System.out.println("Element [3] = " + power[8]);
+        System.out.println("Element [4] = " + power[4]);
+        System.out.println("Element [5] = " + power[3]);
+        System.out.println("Element [6] = " + power[2]);
+        System.out.println("Element [7] = " + power[1]);
+        System.out.println("Element [8] = " + power[0]);
+        }
+    }
