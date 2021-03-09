33712e0e29d81ecfebcc017b1da4977d1301b790
Dziben <56271191+Dziben@users.noreply.github.com>
2021-03-09 03:58:53 +0100

    Create Zwierze.java

---
+++ Klasy Abstrakcyjne/Zwierze.java
@@ -0,0 +1,18 @@
+abstract public class Zwierze
+{
+
+    abstract public void rasa();
+    abstract public void wielkosc();
+    abstract public void rokUrodzenia();
+
+    public void sprawdzCzyOddycha()
+    {
+        System.out.println("Oddycha");
+    }
+
+    public void sprawdzCzyZyje()
+    {
+        System.out.println("Żyje");
+    }
+
+}
