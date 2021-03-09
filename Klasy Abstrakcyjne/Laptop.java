a2bd09ec3335b8054657a924463e79a63d221af9
Dziben <56271191+Dziben@users.noreply.github.com>
2021-03-09 03:58:44 +0100

    Create Laptop.java

---
+++ Klasy Abstrakcyjne/Laptop.java
@@ -0,0 +1,14 @@
+abstract public class Laptop
+{
+
+    abstract public void kartasieciowa();
+    abstract public void dyskSSD();
+    public void RokZlozenia (){
+        System.out.println("Rok złożenia to 2007");
+    }
+    public void SzklanaObudowa(){
+        System.out.println("Tak, z szybą hartowaną.");
+    }
+
+
+}
