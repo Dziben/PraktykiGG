f9f809c93dd83dd1e45cd3d8dd60c60c1344d91c
Dziben <56271191+Dziben@users.noreply.github.com>
2021-03-09 03:58:51 +0100

    Create Samochod.java

---
+++ Klasy Abstrakcyjne/Samochod.java
@@ -0,0 +1,12 @@
+public class Samochod implements Pojazd
+{
+
+    public void skrecWLewo()
+    {
+        System.out.println("Skręt w lewo!");
+    }
+
+    public void skrecWPrawo() {
+        System.out.println("Skręt w prawo!");
+    }
+}
